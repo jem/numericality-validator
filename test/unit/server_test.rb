014f193e82dd050b25049769e5a7cf929f8faf40
Jacob Mattingley <jem@ubalo.com>
2012-11-26 16:21:35 -0800

    Add unit tests demonstrating validator

--- test/unit/server_test.rb
+++ test/unit/server_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ServerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without u_size" do
+    server = Server.new
+    assert !server.save, "saved without u_size"
+  end
+
+  test "should save with numerical u_size" do
+    server = Server.new
+    server.u_size = 5
+    assert server.save, "saved with numerical u_size"
+  end
+
+  test "should not save with non-numeric u_size" do
+    server = Server.new
+    server.u_size = 'a'
+    assert !server.save, "saved with non-numeric u_size"
+  end
 end
